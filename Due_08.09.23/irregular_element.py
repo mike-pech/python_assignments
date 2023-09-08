412b367a4eaa478a4f440121ead09eb46842df7d
Mikhail Pecherkin <m.pecherkin.sas@gmail.com>
2023-09-08 17:18:48 +0300

    Пофикшен парсер в задаче на поиск непоследовательного элемента

--- Due_08.09.23/irregular_element.py
+++ Due_08.09.23/irregular_element.py
@@ -1,8 +1,8 @@
-def parse(input_string: str) -> list:
-    input_string = input_string[1:len(input_string)-1]                # Отбрасывает квадратные скобки, как нулевой и последний символ в вводимой строке 
-    return list(map(int, input_string.split(",")))                    # Map применяет функцию int к каждому элементу генерируемого сплитом списка, а потом получившийся map_object превращается обратно в список
+def parse(input_string: str) -> list[int]:
+    # input_string = input_string[1:len(input_string)-1]                # Отбрасывает квадратные скобки, как нулевой и последний символ в вводимой строке 
+    return list(map(int, input_string.split()))                    # Map применяет функцию int к каждому элементу генерируемого сплитом списка, а потом получившийся map_object превращается обратно в список
 
-def find_irregularities(array: list) -> int | list | str:
+def find_irregularities(array: list) -> int | list[int] | str:
     irregular_index = []
     for n in range(len(array)-1):
         if array[n+1] - array[n] != 1:                                # Если разность следующего члена массива и текущего члена массива не равна 1, то у функции встаёт вопросик
