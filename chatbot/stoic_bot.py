abbc00054ac4280d04221db291fb6e5f5e8315d4
Mikhail Pecherkin <m.pecherkin.sas@gmail.com>
2023-12-18 11:19:55 +0300

    Чатбот на основе TF-IDF готов!

--- chatbot/stoic_bot.py
+++ chatbot/stoic_bot.py
@@ -1,15 +1,8 @@
-# import nltk
-# nltk.download("punkt")
-# nltk.download("wordnet")
-# lemmer = nltk.stem.WordNetLemmatizer()
-# from sklearn.feature_extraction.text import TfidfVectorizer
+from hybridtfidf import HybridTfidf
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 import spacy
 nlp = spacy.load("en_core_web_sm")
-# from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
-from hybridtfidf import HybridTfidf
-
-hybridtfidf = HybridTfidf(threshold=7)
 
 
 PREFIX = "STOIC: "
@@ -17,38 +10,28 @@ GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "howdy", "ghbdtn", "privet
 GOODBYE_INPUTS = ("goodbye", "bye", "quit", "exit", "poka", "mi tawa")
 
 def initialize() -> None:
+    
     with open("./stoic_data.txt", "r") as stoic_file:
-        # temp_tokens = list(nlp(stoic_file.read()).sents)
-        # print(type(temp_tokens[0]))
-        # for sent in temp_tokens:
-        #     sent = " ".join([word for word in nlp(str(sent)) if not word.is_stop])
-        #     sent_tokens.append(sent)
-        sent_tokens = " ".join([token.text for token in nlp(stoic_file.read()) if not token.is_stop])
+        sentences = stoic_file.readlines()
+    replies = sentences
+
+    sent_tokens = []
+    for sent in sentences:
+        lemm_sent = [word.lemma_ for word in nlp(sent) if not word.is_stop and word.text.isalpha()]
+        sent_tokens.append(" ".join(lemm_sent))
+
     print("Initialized successfully!\n_________________________\n\n")
-    return sent_tokens
+    return sent_tokens, replies
 
-# def lemTokens(tokens):
-#     return [lemmer.lemmatize(token) for token in tokens]
+def response(user_response: str, sent_tokens: list, replies: list) -> str:
+    sent_tokens.append(user_response)
 
-# def lemNormalize(text):
-#     remove_punct_dict = dict((ord(punct), None) for punct in punctuation)
-#     return lemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
-
-def cosine_similarity(a, b):
-    return np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
-
-def response(user_response: str, sent_tokens) -> str:
-    sent_tokens = sent_tokens + user_response
-    # TfidfVec = TfidfVectorizer(
-    #     tokenizer = lemNormalize, 
-    #     token_pattern = None,
-    #     stop_words = "english")
-    
-    # tfidf = TfidfVec.fit_transform(sent_tokens) 
+    hybridtfidf = HybridTfidf(threshold=7)
     hybridtfidf.fit(sent_tokens)
-    tfidf = np.array(hybridtfidf.transform(sent_tokens)) 
+
+    tfidf = np.array(hybridtfidf.transform(sent_tokens))
     
-    vals = cosine_similarity(tfidf[-1].reshape(-1, 1), tfidf)
+    vals = cosine_similarity(tfidf[-1].reshape(1, -1), tfidf)
     
     idx = vals.argsort()[0][-2]
     
@@ -59,9 +42,9 @@ def response(user_response: str, sent_tokens) -> str:
     if(req_tfidf == 0):
         robo_response =  "I am sorry! I don't understand you"
     else:
-        robo_response = sent_tokens[idx]
+        robo_response = replies[idx]
         
-    sent_tokens = sent_tokens.replace(user_response, "")
+    sent_tokens.remove(user_response)
     return robo_response
 
 def main() -> None:
@@ -79,12 +62,12 @@ def main() -> None:
         print(f"{PREFIX}You are welcome!")
         main()
     
-    print(response(user_response, sent_tokens))
+    print(response(user_response, sent_tokens, replies))
     main()
 
-# try:
-sent_tokens = initialize()
-main()
-# except Exception as e:
-#     print(e)
-    # quit()
+try:
+    sent_tokens, replies = initialize()
+    main()
+
+except Exception as e:
+    print(e)
